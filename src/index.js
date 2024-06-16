3e7da0185cd648fdb26f6231efcbb50ff12f2af3
luler <34400577+luler@users.noreply.github.com>
2024-06-17 02:15:49 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.io.5201798.xyz": dockerHub,
+  "quay.io.5201798.xyz": "https://quay.io",
+  "gcr.io.5201798.xyz": "https://gcr.io",
+  "k8s-gcr.io.5201798.xyz": "https://k8s.gcr.io",
+  "k8s.io.5201798.xyz": "https://registry.k8s.io",
+  "ghcr.io.5201798.xyz": "https://ghcr.io",
+  "cloudsmith.io.5201798.xyz": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.io.5201798.xyz": dockerHub,
 };
 
 function routeByHosts(host) {
